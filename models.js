b6a974c88c0b72ca81eb3525603ce25c84686d6b
Djabir1007 <jabir.pashaev@gmail.com>
2025-11-20 23:32:40 +0300

    Список моделей телефонов

---
+++ models.js
@@ -0,0 +1,91 @@
+const apple = [
+  "iPhone 12",
+  "iPhone 12 Max",
+  "iPhone 13",
+  "iPhone 13 Max",
+  "iPhone 13 Pro Max",
+  "iPhone 14",
+];
+const inoi = [
+  "INOI 1 Lite",
+  "INOI 5i",
+  "INOI 7 2022",
+  "INOI A62",
+  "INOI A83",
+  "INOI A22",
+];
+
+const nokia = [
+  "Nokia G10",
+  "Nokia G20",
+  "Nokia X10",
+  "Nokia X20",
+  "Nokia C21",
+  "Nokia 5.4",
+];
+
+const oppo = [
+  "OPPO A16",
+  "OPPO A57",
+  "OPPO A74",
+  "OPPO Reno 5",
+  "OPPO Reno 6",
+  "OPPO Reno 7",
+];
+
+const xiaomi = [
+  "Redmi Note 10",
+  "Redmi Note 11",
+  "Redmi 9A",
+  "Mi 11 Lite",
+  "Mi 11",
+  "Poco X3 Pro",
+];
+
+const realme = [
+  "Realme C21",
+  "Realme C25",
+  "Realme 7",
+  "Realme 8",
+  "Realme 9",
+  "Realme GT Master",
+];
+
+const samsung = [
+  "Galaxy A12",
+  "Galaxy A32",
+  "Galaxy A52",
+  "Galaxy S21",
+  "Galaxy S22",
+  "Galaxy M12",
+];
+
+const sony = [
+  "Sony Xperia 10",
+  "Sony Xperia 10 II",
+  "Sony Xperia 5",
+  "Sony Xperia 5 II",
+  "Sony Xperia 1",
+  "Sony Xperia 1 III",
+];
+
+const vivo = [
+  "Vivo Y12",
+  "Vivo Y21",
+  "Vivo Y33s",
+  "Vivo V20",
+  "Vivo V21",
+  "Vivo V23",
+];
+
+export const models = {
+  Apple: apple,
+  INOI: inoi,
+  Nokia: nokia,
+  Oppo: oppo,
+  Xiaomi: xiaomi,
+  Realme: realme,
+  Samsung: samsung,
+  Sony: sony,
+  Vivo: vivo,
+};
